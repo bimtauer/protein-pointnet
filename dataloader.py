280eda178015ad3ddf845212c37f2f51b920e7fc
Tim Bauer <ti7822ba-s@student.lu.se>
2019-01-28 17:45:23 +0100

    Clean Repo

--- dataloader.py
+++ dataloader.py
@@ -17,14 +17,14 @@ ATP = sample_all(os.path.join(path, atp), 1000)
 CLR = sample_all(os.path.join(path, clr), 1000)
 SAM = sample_all(os.path.join(path, sam), 1000)
 
-def return_samples():
+def return_samples(what):
     bindings = [ATP, CLR, SAM]
     names = [atp, clr, sam]
     for_each_ligand = {}
     for binding, name in zip(bindings, names):
         all_samples = []
         for key, values in binding.items():
-            points = np.array(values['sample'])
+            points = np.array(values[what])
             all_samples.append(points)
         all_samples = np.array(all_samples)
         for_each_ligand[name] = all_samples
